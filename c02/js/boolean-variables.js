3290601356bfe854051b909cf233cbc6a401dcce
Joseph Phillipson <joey.phillipson1@hotmail.com>
2021-12-08 11:42:13 +0000

    Boolean variables

---
+++ c02/js/boolean-variables.js
@@ -0,0 +1,10 @@
+var inStock;
+inStock = true;
+var shipping;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+elStock.className = inStock;
+
+var elShip = document.getElementById('shipping');
+elShip.className = shipping;
