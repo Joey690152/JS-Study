b5dba94ebac919bff81836ddef152e00ed9a3b40
Joseph Phillipson <joey.phillipson1@hotmail.com>
2021-12-08 11:51:51 +0000

    shorthand variables

---
+++ c02/js/shorthand-variable.js
@@ -0,0 +1,12 @@
+// long way
+
+// var price = 5;
+// var quantity = 14;
+// var total = price * quantity;
+
+// shorthand 
+var price = 5, quantity = 14;
+total = price * quantity;
+
+var el = document.getElementById('cost');
+el.textContent = '£' + total;
