ad2a7b07f61d64a720a5703d629e4af8bc11727e
Joseph Phillipson <joey.phillipson1@hotmail.com>
2021-12-08 11:19:40 +0000

    String with quotes

---
+++ c02/js/string-with-quotes.js
@@ -0,0 +1,9 @@
+var title;
+title = "Molly's Special offers"
+var message;
+message = '<a href=\"sale.html\">25% off!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+var elNote = document.getElementById('note');
+elNote.innerHTML = message;
